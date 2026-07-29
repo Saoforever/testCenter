a354716fb70694a128569f8f3027da80c78e61cb
Sao <238361601+Saoforever@users.noreply.github.com>
2026-07-29 19:23:55 -0400

    updated

--- phase1_conditionals.py
+++ phase1_conditionals.py
@@ -1,8 +1,8 @@
 age = 5
 
-if age < 13:
+if age < 13:                   # Asking if under _ then _
     print("child")
-elif age >= 13 and age <= 19:
+elif age >= 13 and age <= 19:  # Asking else if over _ and under _ then _
     print("teenager")
-else:
+else:                          # Then print this (not fitting the other criteria) 
     print("adult")
--- phase1_variable.py
+++ phase1_variable.py
@@ -1,12 +1,12 @@
-name = "Mike"
-age = 27
-fav_number = 7.7
-likes_coding = True
-
-print(name)
-print(age)
-print(fav_number)
-print(likes_coding)
+name = "Mike"               # this a string (name is variable and "Mike" the value)
+age = 27                    # this an integer 
+fav_number = 7.7            # this is a float
+likes_coding = True         # this a bool(ean)
+                            # REMEMBER toolbox = tool 
+print(name)                 # WHO
+print(age)                  # YOU   
+print(fav_number)           # ARE! 
+print(likes_coding)   
 print(f"Hi im {name}, I'm {age}, my favorite number is {fav_number}, and do I like coding? I think thats... {likes_coding} ")
 
 print(type(name))
